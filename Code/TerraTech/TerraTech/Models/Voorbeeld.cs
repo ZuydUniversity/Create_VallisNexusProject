ab1458db45765309a7ff56da2e907786a813b1f9
Excali8ur <rianne.boumans@zuyd.nl>
2026-06-02 23:44:12 +0200

    Name changed TechTerra -> VallisNexus Added class "Voorbeeld"

---
+++ Code/TerraTech/TerraTech/Models/Voorbeeld.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace VallisNexus.Models
+{
+    public class Voorbeeld
+    {
+        //Attribuut / Eigenschap        
+        private int getal;
+        //Property voor getal
+        public int Getal
+        {
+            get { return getal; }
+            set
+            {
+                if (value > 0)
+                {
+                    getal = value;
+                }
+            }
+        }
+
+        //Read-only Property, zonder los attribuut
+        public string Tekst { get; private set; }
+
+        //Constructor
+        public Voorbeeld()
+        {
+            Tekst = "Dit is een standaard tekst";
+        }
+
+        public void PasTekstAan(string tekst)
+        {
+            this.Tekst = $"{getal}: {tekst}";
+        }
+
+    }
+}
--- Code/TerraTech/TerraTech/Program.cs
+++ Code/TerraTech/TerraTech/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace TerraTech
+﻿using VallisNexus.Models;
+
+namespace VallisNexus
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Voorbeeld voorbeeld = new Voorbeeld();
+
+            Console.WriteLine("Geef een getal:");
+            int getal = int.Parse(Console.ReadLine());
+
+            voorbeeld.Getal = getal;
+
+            Console.WriteLine(voorbeeld.Tekst);
+            voorbeeld.PasTekstAan("Nieuwe Tekst...");
+            Console.WriteLine(voorbeeld.Tekst);
         }
     }
 }
